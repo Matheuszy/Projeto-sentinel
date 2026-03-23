c19fdfa3baf82968dd8c6b727fc5c0208607f564
Matheus Carlos <169002682+Matheuszy@users.noreply.github.com>
2026-03-22 21:49:26 -0300

    Modify transactions table schema in migration SQL

--- Projeto-Sentinel/sentinel-api/src/main/resources/db/migration/V1__create_transactions_table.sql
+++ Projeto-Sentinel/sentinel-api/src/main/resources/db/migration/V1__create_transactions_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS transactions (
     id BIGSERIAL PRIMARY KEY,
-    transaction_id VARCHAR(255) NOT NULL,
+    transaction_id BIGINT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     merchant_category BIGINT,
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+
 
 );
